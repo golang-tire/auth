8a62476ef9b2cd653dbf09a4f44537ece0cbf31c
mohsen <mohsen.mkh88@gmail.com>
2020-11-28 20:48:15 +0100

    limit to swagger files

--- build/to_swagger.sh
+++ build/to_swagger.sh
@@ -3,7 +3,7 @@
 PACKAGE_NAME="auth";
 OUTPUT_DIR="internal/proto/v1"
 
-for f in $(find -type f -name "*.json"); do
+for f in $(find -type f -name "*.swagger.json"); do
   output_filename=$(basename $f)
   tire swagger-to-go $f --pkg $PACKAGE_NAME --out $OUTPUT_DIR/$output_filename.go
 done
